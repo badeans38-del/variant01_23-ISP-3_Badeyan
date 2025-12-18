26712d2932ea776096862c362041226c7974765b
badeans38-del <badeans38@gmail.com>
2025-12-18 20:17:25 +0300

    Add files via upload

---
+++ Lab1_Variant1_23ISP3_Badeyan.java
@@ -0,0 +1,31 @@
+public class Lab1_Variant1_23ISP3_Badeyan {
+    public static void main(String[] args)
+    {
+        // Задание 1
+        double dlina=120.75;
+        double shirina=80.25;
+        double perimetr=2*(dlina+shirina);
+        double ploshad= dlina*shirina;
+        System.out.printf("Периметр участка=" + perimetr);
+        System.out.printf("Площадь участка=" + ploshad);
+        System.out.println("" );
+
+
+        // Задание 2
+        int kolichestvoStudentov=25;
+        System.out.println("Кол-во студентов=" + kolichestvoStudentov);
+        System.out.println("" );
+
+        //Задание 3
+        int a=100;
+        int b=200;
+        b=b-a;
+        a=a+b;
+        System.out.println("Значения после обмена: a="+a+",b="+b);
+
+
+
+
+
+    }
+}
